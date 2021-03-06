f924876abc32174935aede4c47b00578e896d983
guxinyu <975631766@qq.com>
2021-03-06 21:54:35 +0800

    wwww

---
+++ config/api.config.js
@@ -0,0 +1,11 @@
+layui.use(['context'],function(){
+    const context = layui.context;
+    context.put("hostUrl","http://localhost:8081")
+    // if (sessionStorage.getItem("token")==null){
+    //     parent.window.location='../../login.html'
+    // }
+
+})
+
+
+
